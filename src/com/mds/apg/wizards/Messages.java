9f957a535661c3ee559a385e972d2c03c989ec31
Takashi Okamoto <toraneko at gmail.com>
2012-01-17 16:41:55 +0900

    Externalize strings.

---
+++ src/com/mds/apg/wizards/Messages.java
@@ -0,0 +1,45 @@
+package com.mds.apg.wizards;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "com.mds.apg.wizards.messages"; //$NON-NLS-1$
+	public static String AndroidPgProjectCreationPage_Description;
+	public static String AndroidPgProjectCreationPage_Title;
+	public static String AndroidPgProjectCreationPage_with_jQueryMobile;
+	public static String AndroidPgProjectCreationPage_with_SenchaTouch;
+	public static String AndroidPgProjectNewWizard_Error_Case_Variant_Exists;
+	public static String AndroidPgProjectNewWizard_Title;
+	public static String PageInitContents_ErrorIndexHTMLNotFound;
+	public static String PageInitContents_ErrorLocationEmpty;
+	public static String PageInitContents_ErrorLocationInvalid;
+	public static String PageInitContents_ErrorNotFoundJQMDir;
+	public static String PageInitContents_ErrorNotFoundPhoneGapJSFile;
+	public static String PageInitContents_ErrorNotFoundSenchaDir;
+	public static String PageInitContents_Location;
+	public static String PageInitContents_MinimalProjectTooltip;
+	public static String PageInitContents_MnimalProject;
+	public static String PageInitContents_ProjectContents;
+	public static String PageInitContents_PureImport;
+	public static String PageInitContents_PureImportTooltip;
+	public static String PageInitContents_SpecifiedSourceDir;
+	public static String PageInitContents_SpecifiedSourceDirTooltip;
+	public static String PageInitContents_UseExample;
+	public static String PageInitContents_UseExampleTooltip;
+	public static String PagePhonegapPathSet_BuildIn;
+	public static String PagePhonegapPathSet_BuildInTooltip;
+	public static String PagePhonegapPathSet_Configuration;
+	public static String PagePhonegapPathSet_EnterPhoneGapPath;
+	public static String PagePhonegapPathSet_EnterPhoneGapPathTooltip;
+	public static String PagePhonegapPathSet_ErrorDirNameEmpty;
+	public static String PagePhonegapPathSet_PhoneGapPath;
+	public static String PagePhonegapPathSet_StatusDirEmpty;
+	public static String PagePhonegapPathSet_StatusDirNameEmpty;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
